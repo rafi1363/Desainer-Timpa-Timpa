10fd687d5d39db3b0868593ac067f7e24762e452
rafi1363 <ryuta130603@gmail.com>
2025-09-22 09:37:35 +0700

    api revision

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server", // atau 'hybrid'
+  adapter: netlify(),
+});
